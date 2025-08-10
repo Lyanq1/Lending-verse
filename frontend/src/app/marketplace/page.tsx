5fe219dbaedf16d1a53f6d409850483ae166a265
lyanq <anhquan2004162@gmail.com>
2025-08-11 00:50:07 +0700

    Modify lenders

--- frontend/src/app/marketplace/page.tsx
+++ frontend/src/app/marketplace/page.tsx
@@ -90,7 +90,7 @@ export default function Marketplace() {
             Request Loan
           </Link>
           <Link 
-            href="/offers/create" 
+            href="/loans/create" 
             className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors"
           >
             Create Offer
